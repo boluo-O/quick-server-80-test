7f14d9fc590ca799e54339b13a4ae42a05907083
boluo <xiangwuz@outlook.com>
2024-03-18 21:47:21 +0800

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http")
+
+http.createServer((req, res) => {
+    //  设置cors跨域
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type")
+    res.setHeader("Content-Type", "application/json")
+    console.log("req.url", req.url)
+
+    res.statusCode = 200
+    res.end("server start success on 80 port")
+}).listen(80, "127.0.0.1")
